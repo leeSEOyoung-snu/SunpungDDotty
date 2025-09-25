fb9b6af341072c6c776e6a1601f1b1dcbfa28009
leeSEOyoung <seosuper01@snu.ac.kr>
2025-09-25 15:11:23 +0900

    [Receive/feat] Player 점프 기능 추가

--- Assets/Scripts/Receive/ReceivePlayerController.cs
+++ Assets/Scripts/Receive/ReceivePlayerController.cs
@@ -4,18 +4,25 @@ using UnityEngine;
 public class ReceivePlayerController : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private float moveSpeed;
-
-    private void FixedUpdate()
+    [SerializeField] private float moveSpeed, jumpForce;
+    
+    private void Update()
     {
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
             Move(Vector2.left);
-        else if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
             Move(Vector2.right);
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space)) 
+            Jump();
     }
 
     private void Move(Vector2 direction)
     {
         rb.AddForce(direction * moveSpeed, ForceMode2D.Impulse);
+    }
+
+    private void Jump()
+    {
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
     }
 }
