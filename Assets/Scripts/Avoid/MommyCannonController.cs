aac51693791c79d2cda9ab7f21d2cac62e8d4207
leeSEOyoung-snu <seosuper01@snu.ac.kr>
2025-09-27 05:42:38 +0900

    [Avoid/feat] 엄마 대포 추가

--- Assets/Scripts/Avoid/MommyCannonController.cs
+++ Assets/Scripts/Avoid/MommyCannonController.cs
@@ -1,18 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class MommyCannonController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Programmer")]
+    [SerializeField] private bool moveX;
+    [SerializeField] private bool moveY;
+    [SerializeField] private float moveRange;
+    [SerializeField] private float directionFactor;
+    
+    [Header("Level Design")]
+    [SerializeField] private float moveSpeed;
+
+    private float _center;
+
+    private void Awake()
     {
-        
+        _center = moveX ? transform.position.x : transform.position.y;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        float criteria = moveX ? transform.position.x - _center : transform.position.y - _center;
         
+        directionFactor = 
+            criteria >= moveRange ? -1f 
+            : criteria <= moveRange * -1f ? 1f 
+            : directionFactor;
+        
+        Vector2 direction = new Vector2(moveX ? 1 : 0, moveY ? 1 : 0);
+        transform.Translate(direction * directionFactor * Time.deltaTime * moveSpeed);
     }
 }
