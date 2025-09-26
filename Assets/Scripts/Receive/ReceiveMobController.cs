c62f61ff3e9abf96c7fdd3667ece4967e215cf73
leeSEOyoung <seosuper01@snu.ac.kr>
2025-09-26 15:52:16 +0900

    [Receive/feat] collision 대상에 따른 mob의 행동 지정

--- Assets/Scripts/Receive/ReceiveMobController.cs
+++ Assets/Scripts/Receive/ReceiveMobController.cs
@@ -5,6 +5,7 @@ public class ReceiveMobController : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb;
     
+    private bool _isDead = false;
     private int _collisionCnt = 0;
     
     private const int MaxCollisionCnt = 4;
@@ -20,9 +21,26 @@ public class ReceiveMobController : MonoBehaviour
         float velX = UnityEngine.Random.Range(-1f * VelocityXRange, VelocityXRange);
         rb.velocity = new Vector2(velX, DefaultVelocityY);
     }
-
-    private void OnCollisionEnter(Collision other)
+    
+    private void OnCollisionEnter2D(Collision2D collision)
     {
+        if (_isDead) return;
         
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            _collisionCnt++;
+            if (_collisionCnt == MaxCollisionCnt)
+            {
+                _isDead = true;
+            }
+            else if (_collisionCnt < MaxCollisionCnt)
+            {
+                SetRandomVelocity();
+            }
+        }
+        else if (collision.gameObject.CompareTag("Ground"))
+        {
+            _isDead = true;
+        }
     }
 }
